9d9629e98e728e624321ff51016af0027d63dac7
Khaled <loudiharf@gmail.com>
2023-11-01 20:20:46 -0400

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by loudi on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
